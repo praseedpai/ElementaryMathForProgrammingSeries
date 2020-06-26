15383cded32130d9e78cef7a3b80a14899c49053
praseedpai <praseedp@gmail.com>
2020-06-26 17:45:09 +0530

    saved

--- AlgebraNArith/SinCosScratch/SinCos.cpp
+++ AlgebraNArith/SinCosScratch/SinCos.cpp
@@ -1,100 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-
-/////////////////////////////////////////////
-//
-//
-//
-//
-//
-//
-//
 double m_abs( double arg ) {
-
-         if ( arg < 0.0 )
-			 arg = -1*arg;
-
-         return arg;
+	 if ( arg < 0.0 ) { arg = -1*arg;}
+	 return arg;
 }
 
-////////////////////////////////////////////////////
-//
-//
-//
-//
-double m_dtor(double x ) {
+double m_dtor(double x ) { return ( x * 3.14159/180.0 ); }
 
-
-  
-   return ( x * 3.14159/180.0 );
-
-}
-
-
-
-///////////////////////////////////////////
-//
-//
-//
-//  Program to Compute Sin and cosine 
-//
-//
-//
-//
-//
-
-double m_sin(double arg ) 
-{
-
+double m_sin(double arg ) {
 	double term = arg;
-    double i = 1;
-    double x2 = arg*arg;
+    	double i = 1;
+    	double x2 = arg*arg;
 	double tsin = arg;
-
-	
-	while ( m_abs(term) > 0.0000001 )
-	{
-
-         i = i + 2;
-         term = -term*x2/(i*(i-1));
-		 tsin = tsin + term; 
-
-	
+	while ( m_abs(term) > 0.0000001 ){
+        	 i = i + 2;
+       	         term = -term*x2/(i*(i-1));
+         	 tsin = tsin + term; 
 	}
-
-    return tsin;
-
-
-
+	return tsin;
 }
 
+double m_cos(double arg ) { return m_sin( (2*3.14159/4.0) - arg ); }
 
-
-int main() 
-{
-
-   double dval= 0.0;
-   long sval = 0;
-
-   printf("Enter the value of sin in degrees \n");
-   scanf("%d",&sval);
-
-   dval = (float) sval;
-   printf("%g ==== \n",dval);
-   
-
-    
-
+int main(int argc, char **argv) {
+   double dval = (argc == 1 )? 45.0 : atof(argv[1]);
    printf("\n\n\n\nValue is %e\n\n" ,dval*3.14159/180.0);
-   printf("my sine = %f\t\t\tmath.h sin = %f\n",m_sin(m_dtor(dval)),sin(m_dtor(dval)));
-   printf("my cosine = %f\t\t\tmath.h cosine = %f\n",m_sin(m_dtor(90-dval)),sin(m_dtor(90-dval)));
+   printf("my sine = %f\t\t\tmath.h sin = %f\n",
+		m_sin(m_dtor(dval)),sin(m_dtor(dval)));
+   printf("my cosine = %f\t\t\tmath.h cosine = %f\n",
+		m_cos(m_dtor(dval)),cos(m_dtor(dval)));
    
-
-
-
-
-
-
-
 }
